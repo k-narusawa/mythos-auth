2ee5db9977c6fda5b1854955919f539500618679
k-narusawa <swkei320@gmail.com>
2024-01-08 12:11:53 +0900

    feat: 🎸 メール送信部分実装

--- adapter/controllers/mail_controller.go
+++ adapter/controllers/mail_controller.go
@@ -1,10 +1,15 @@
 package controllers
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2/types"
 	"github.com/labstack/echo/v4"
 )
 
@@ -20,10 +25,53 @@ func (mc *MailController) Send(c echo.Context) error {
 	if err := c.Bind(ve); err != nil {
 		return c.String(http.StatusBadRequest, "Bad Request")
 	}
-
-	fmt.Fprintf(os.Stdout, "Recipient: %v\n", ve.Recipient)
-	fmt.Fprintf(os.Stdout, "To: %v\n", ve.To)
 	fmt.Fprintf(os.Stdout, "TemplateType: %v\n", ve.TemplateType)
+
+	ctx := context.Background()
+	cfg, err := config.LoadDefaultConfig(ctx,
+		config.WithEndpointResolver(aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+			return aws.Endpoint{URL: "http://localhost:8005"}, nil
+		})))
+	cfg.Region = "ap-northeast-1"
+
+	if err != nil {
+		// error handling
+	}
+
+	var (
+		fromEmailAddress = "mythos@example.com"
+		toEmailAddress   = ve.Recipient
+		subject          = ve.Subject
+		body             = ve.Body
+	)
+
+	client := sesv2.NewFromConfig(cfg)
+
+	input := &sesv2.SendEmailInput{
+		FromEmailAddress: &fromEmailAddress,
+		Destination: &types.Destination{
+			ToAddresses: []string{toEmailAddress},
+		},
+		Content: &types.EmailContent{
+			Simple: &types.Message{
+				Body: &types.Body{
+					Text: &types.Content{
+						Data: &body,
+					},
+				},
+				Subject: &types.Content{
+					Data: &subject,
+				},
+			},
+		},
+	}
+
+	res, err := client.SendEmail(ctx, input)
+	if err != nil {
+		// error handling
+	}
+	fmt.Println(res.MessageId)
+	fmt.Println("success!")
 
 	return c.NoContent(http.StatusNoContent)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,20 @@ go 1.20
 require github.com/spf13/viper v1.17.0
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.24.0 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.26.2 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.16.13 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.10 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.10.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.10.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sesv2 v1.24.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.18.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.21.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.26.6 // indirect
+	github.com/aws/smithy-go v1.19.0 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
